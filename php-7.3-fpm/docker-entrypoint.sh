989dfe98967a8b6a18fe4c7782950eb389482398
Amant Arnaud <contact@arnaudamant.fr>
2020-06-24 14:14:42 +0200

    update xdebug and add Blackfire

--- php-7.3-fpm/docker-entrypoint.sh
+++ php-7.3-fpm/docker-entrypoint.sh
@@ -4,4 +4,8 @@
     docker-php-ext-enable xdebug && \
     echo "Xdebug is enabled"
 
+[ "$PHP_ENABLE_BLACKFIRE" = "true" ] && \
+    printf "extension=blackfire.so\nblackfire.agent_socket=tcp://blackfire:8707\n" > $PHP_INI_DIR/conf.d/blackfire.ini && \
+    echo "Xdebug is enabled"
+
 exec "$@"
