14652a5ac8a2e259ef82a0974b649d651e01b062
Amant Arnaud <contact@arnaudamant.fr>
2020-11-03 15:43:53 +0100

    optimize Dockerfile for Magento

--- php-7.4-fpm/docker-entrypoint.sh
+++ php-7.4-fpm/docker-entrypoint.sh
@@ -4,4 +4,9 @@
     docker-php-ext-enable xdebug && \
     echo "Xdebug is enabled"
 
+[ "$MAGENTO_DIR" = "true" ] \
+    && chown -R www-data var \
+    && chown -R www-data generated \
+    && chown -R www-data pub/static \
+
 exec "$@"
