d7a473f37a42426da232b8d3d5350bf5c3917ab8
Amant Arnaud <contact@arnaudamant.fr>
2021-03-01 11:07:25 +0100

    update php 5.6

---
+++ php-5.6-fpm/docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+[ "$PHP_ENABLE_XDEBUG" = "true" ] && \
+    docker-php-ext-enable xdebug && \
+    echo "Xdebug is enabled"
+
+[ "$MAGENTO_DIR" = "true" ] \
+    && chown -R www-data var/cache \
+
+exec "$@"
